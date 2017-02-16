de4c8429776b48b33189e654153fb6655a73801f
GIECOM <GIECOM>
2017-02-16 17:05:54 -0500

    castear float

--- src/firshclass/FirshClass.java
+++ src/firshclass/FirshClass.java
@@ -4,12 +4,15 @@ package firshclass;
 public class FirshClass {
 
     public static void main(String[] args) {
-        Motor a=new Motor(1,2);
-        //send values
-        System.out.println("Values in Fractions.");
-        System.out.println(a.a(1, 2));
-        System.out.println("Controls.");
+//        float a=(float)2.2; //cast float 
+        new Motor((float)2.2);
         
+        
+        //send values
+//        System.out.println("Values in Fractions.");
+//        System.out.println(a.a(1, 2));
+//        System.out.println("Controls.");
+//        System.out.println(a.b(5, 0));
     }
     
 }
--- src/firshclass/Motor.java
+++ src/firshclass/Motor.java
@@ -19,14 +19,29 @@ public class Motor {
     public void setB(int b) {
         this.b = b;
     }
-
-    public Motor(int a, int b) {
-        this.a = a;
-        this.b = b;
+    //construc area
+    public Motor(int a){
+        System.out.println(a);
     }
-
+    public Motor(float a){
+        System.out.println("..."+a);
+    }
     
-    public String a(int getA, int getB){
-        return "Fra: "+a+"/"+b;
-    }
+//    public String a(int getA, int getB){
+//        return "Fra: "+a+"/"+b;
+//    }
+//    
+//    public String b(int getA,int getB){
+//        int a=getA;
+//        int b=getB;
+//        String c="";
+//        if(a<0){
+//            c="1";
+//        }else if(b<0){
+//            c="1";
+//        }else{
+//            c=a+"/"+b;
+//        }
+//        return c;
+//    }
 }
