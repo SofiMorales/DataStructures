ffacae4106f7fd687474fe4f7dfd679b67a77c6d
MitchAguilar <madialeja06@gmail.com>
2017-03-01 21:03:46 -0500

    coments

--- src/ter/A.java
+++ src/ter/A.java
@@ -4,6 +4,7 @@ public class A {
 
     private int a;
     private int b;
+    private float c;
 
     public int getA() {
         return a;
@@ -25,25 +26,30 @@ public class A {
         this.b = b;
     }
 
+    //nulo sin valor
     public A() {
         this.b = 1;
     }
 
+    //que recibe un entero
     public A(int a) {
         this.b = a;
     }
 
+    //que recibe un doble
     public A(double a) {
         this.b = (int) a;
     }
 
+    //que recibe 2 parametros
     public A(int a, int b) {
         int c = a(a, b);
         this.a = a / c;
         this.b = b / c;
     }
 
-    public A simplifi(int a, int b) {
+    //que retorna objeto de los valores
+    public A a2(int a, int b) {
         A z = null;
         int c = a(a, b);
         this.a = a / c;
@@ -53,6 +59,7 @@ public class A {
         return z;
     }
 
+    //que simplifica
     public int a(int a, int b) {
         while (a != b) {
             if (a < b) {
@@ -64,6 +71,28 @@ public class A {
         return (a);
     }
 
+    //que recibe tres valores en ese caso, mixtos
+    public A(int a, int b, int c) {
+        a = a * c;
+        a += b;
+        float a2 = a;
+        float c2 = c;
+        this.c = (float) (a2 / c2);
+    }
+    //que hace un objeto con los 3 valores
+    public A a3(int a, int b, int c) {
+        A z=null;
+        a = a * c;
+        a += b;
+        float a2 = a;
+        float c2 = c;
+        this.c = (float) (a2 / c2);
+        z.a=this.a;
+        z.b=this.b;
+        z.c=this.c;
+        return z;
+    }
+    //no sé por qué lo puse
     public A s(A a, A b) {
         A z = null;
         if (a.b == 0 && b.b == 0) {
@@ -71,33 +100,25 @@ public class A {
         }
         return z;
     }
-    
-    public A(int a,int b,int c){
-        a=a*c;
-        a+=b;
-        System.out.println(a);
-        System.out.println(c);
-        float a2=a;
-        float c2=c;
-        float f2=(float)(a2/c2);
-    }
-
+    //un monstruoso to string()
     @Override
     public String toString() {
-        String c = "";
-        if (b == 1 && a == 0) {
-            c += ((float) b) + "";
+        String d = "";
+        if (c > 0) {
+            d += "value is: " + c;
+        } else if (b == 1 && a == 0) {
+            d += "value is: " + (((float) b) + "");
         } else if (a == 0 && b == 0) {
-            c += a + "." + b;
+            d += "value is: " + (a + "." + b);
         } else if (a == 0 && b < 0) {
-            c += b + "." + a;
+            d += "value is: " + (b + "." + a);
         } else if (a < 0 && b > 0) {
-            double a2=a/b;
-            c += "value is: " + a2+"bitch";
+            double a2 = a / b;
+            d += "value is: " + a2;
         } else {
-            c += "value is: " + a + "/" + b;
+            d += "value is: " + a + "/" + b;
         }
-        return c;
+        return d;
     }
 //    public int s(int a,int b){
 //        
--- src/ter/Main.java
+++ src/ter/Main.java
@@ -3,14 +3,36 @@ package ter;
 public class Main {
 
     public static void main(String[] args) {
-//        A a = new A(8, 3);
-//        A b = new A(0, 0);
-//        System.out.println(a);
-//        System.out.println(b);
-           A a=new A(2, 2, 5);
-
-//        MotorM b = new MotorM(10, 5);
-//        System.out.println(a.s(a,b));
+        A a=new A();
+        A b=new A(0);
+        A c=new A(-4);
+        A d=new A(-10);
+        A e=new A(2,2,5);
+        A f=new A();
+        A g=new A();
+        A h=new A();
+        A i=new A();
+        A j=new A();
+        A k=new A();
+        
+        System.out.println("Begin...");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        
+        
+        System.out.println("suma");
+       A a2=new A(5,7);
+       A a3=new A(-5,12);
+       A a4=new A();
+        System.out.println(a4.s(a2, a3));
+//        System.out.println(g);
+//        System.out.println(h);
+//        System.out.println(i);
+//        System.out.println(j);
+//        System.out.println(k);
     }
-
 }
