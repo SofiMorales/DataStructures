0507ca6fd7d899989acbcd1a8a56c1e212e4ea28
MitchAguilar <madialeja06@gmail.com>
2017-03-06 07:51:14 -0500

    new code

--- src/ter2Ver2/A.java
+++ src/ter2Ver2/A.java
@@ -3,6 +3,9 @@ package ter2Ver2;
 public class A {
     public static void main(String[] args) {
         System.out.println("Begin...");
+        B a=new B();
+        System.out.println("a= "+a+" Value is "+a.getC());
+        
     }
     
 }
--- src/ter2Ver2/B.java
+++ src/ter2Ver2/B.java
@@ -3,7 +3,8 @@ package ter2Ver2;
 public class B {
 
     private int a = 0, b = 0;
-
+    private double c = 0;
+    //get and set
     public int getA() {
         return a;
     }
@@ -20,16 +21,43 @@ public class B {
         this.b = b;
     }
 
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+    /* content consturc*/
+    //constructor basio
     public B() {
-        
+        setC((double)(0));
     }
-    public B(int a){
-        
+    //constructor con un entero
+    public B(int a) {
+
     }
-    public B(double a){
-        
+    //constructor con un double
+    public B(double a) {
+
     }
-    public B(int a,int b){
-        
+    //constructor con dos valores
+    public B(int a, int b) {
+
+    }
+    //to string de B
+    @Override
+    public String toString() {
+        if(a==0||b==0){
+            return a+"";
+        }
+        return "";
+    }
+    
+    /*content simplify*/
+    public int a(int a,int b){
+        int a1=a;
+        int b1=b;
+        while
     }
 }
